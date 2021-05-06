aacc87d63d68694926e449c7ae4e3e7160b58f97
Lucas Neves <luucasmeedeiros@gmail.com>
2021-05-06 00:53:14 -0300

    feat: criacao do projeto em java 8 e springboot

---
+++ src/test/java/br/com/lojabackend/LojaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lojabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
